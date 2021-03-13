90546a71df71e7c13813adc643bf3cc5b4fd053a
JeffreyDotDev <jeffrey@jeffrey.dev>
2021-03-13 21:31:00 +0100

    Make the package available for Vue 3

--- src/directives/vue-clear-on-disable.js
+++ src/directives/vue-clear-on-disable.js
@@ -1,41 +1,44 @@
-export default {
-  inserted(element) {
-    // Create a new MutationObserver to watch the HTMLElement for changes
-    new MutationObserver(mutations => {
-      // Loop through all mutations once the callback is called
-      mutations.forEach(mutation => {
-        const target = mutation.target;
+function inserted(element) {
+  // Create a new MutationObserver to watch the HTMLElement for changes
+  new MutationObserver(mutations => {
+    // Loop through all mutations once the callback is called
+    mutations.forEach(mutation => {
+      const target = mutation.target;
 
-        // Check if the mutation is a disabled change and if the target is now disabled
-        if (mutation.attributeName === 'disabled' && target.disabled) {
-          // Check for the kind of HTMLElement, clear it's value and trigger a input or change event
-          switch (target.tagName) {
-            case 'INPUT':
-              switch (target.type) {
-                case 'text':
-                  target.value = '';
-                  target.dispatchEvent(new CustomEvent('input'));
-                  break;
+      // Check if the mutation is a disabled change and if the target is now disabled
+      if (mutation.attributeName === 'disabled' && target.disabled) {
+        // Check for the kind of HTMLElement, clear it's value and trigger a input or change event
+        switch (target.tagName) {
+          case 'INPUT':
+            switch (target.type) {
+              case 'text':
+                target.value = '';
+                target.dispatchEvent(new CustomEvent('input'));
+                break;
 
-                case 'checkbox':
-                  target.checked = false;
-                  target.dispatchEvent(new CustomEvent('change'));
-                  break;
-              }
-              break;
+              case 'checkbox':
+                target.checked = false;
+                target.dispatchEvent(new CustomEvent('change'));
+                break;
+            }
+            break;
 
-            case 'SELECT':
-              target.value = '';
-              target.dispatchEvent(new CustomEvent('change'));
-              break;
+          case 'SELECT':
+            target.value = '';
+            target.dispatchEvent(new CustomEvent('change'));
+            break;
 
-            case 'TEXTAREA':
-              target.value = '';
-              target.dispatchEvent(new CustomEvent('input'));
-              break;
-          }
+          case 'TEXTAREA':
+            target.value = '';
+            target.dispatchEvent(new CustomEvent('input'));
+            break;
         }
-      })
-    }).observe(element, {attributes: true});
-  }
-};
+      }
+    })
+  }).observe(element, {attributes: true});
+}
+
+export default {
+  inserted,
+  mounted: inserted
+}
